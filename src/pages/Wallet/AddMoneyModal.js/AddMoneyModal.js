4fedb45df95c5a1e5a5435c84c41107f848b582a
Sunil <btwitsbista@gmail.com>
2023-05-08 12:27:58 +0545

    added select user field in add money modal

--- src/pages/Wallet/AddMoneyModal.js/AddMoneyModal.js
+++ src/pages/Wallet/AddMoneyModal.js/AddMoneyModal.js
@@ -4,18 +4,49 @@ import WalletPng from "../../../assets/img/wallet.png";
 
 function AddMoneyModal({ closeModal }) {
   const [addMoney, setAddMoney] = useState(0);
+  const [selectUser, setSelectUser] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(addMoney);
+    console.log(addMoney, selectUser);
     closeModal();
   };
+
+  const options = [
+    {
+      value: "nothing",
+      name: "Please, Select the User",
+    },
+    {
+      value: "sunil",
+      name: "Sunil Bista",
+    },
+    {
+      value: "gaurab",
+      name: "Gaurab Chand",
+    },
+    {
+      value: "suman",
+      name: "Suman Basnet",
+    },
+  ];
+
   return (
     <Modal title="Add Money to Wallet">
       <div className="w-full flex justify-center mb-4">
         <img src={WalletPng} alt="" className="w-48 h-w-48 object-contain" />
       </div>
       <form onSubmit={handleSubmit}>
+        <div class="form-group">
+          <select
+            className="form-control"
+            onChange={(e) => setSelectUser(e.target.value)}
+          >
+            {options.map((option) => (
+              <option value={option.value}>{option.name}</option>
+            ))}
+          </select>
+        </div>
         <div class="form-group">
           <input
             type="text"
